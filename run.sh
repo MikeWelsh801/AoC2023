7a981724cf2cb875df4476fae388b249561ea89c
MICHAEL WELSH <mikewelsh801@gmail.com>
2023-12-15 13:26:28 -0700

    Copied .gitignore and run script from last year's repo.

---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+path=$1"/rust_sol"
+
+if [ ! -d $path ]; then
+    echo "$path does not exist."
+    echo "Please enter file with rust solution."
+else
+    cd $path
+    cargo run
+    cd ../..
+fi
+    
