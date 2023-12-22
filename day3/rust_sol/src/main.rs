528aebac6df6a60262eeb1944da7c5654cc89f28
MICHAEL WELSH <mikewelsh801@gmail.com>
2023-12-22 16:46:55 -0700

    Added day 3, part 2 solution.

--- day3/rust_sol/src/main.rs
+++ day3/rust_sol/src/main.rs
@@ -18,6 +18,13 @@ impl Part {
         }
     }
 
+    fn is_here(&self, row: usize, col: usize) -> bool {
+        if self.line == row && col >= self.col_start && col <= self.col_end {
+            return true;
+        }
+        false
+    }
+
     fn is_valid(&self, grid: &[Vec<char>]) -> bool {
         // check up and down for every digit
         for j in self.col_start..=self.col_end {
@@ -95,6 +102,88 @@ fn main() {
 
     println!("Answer 1: {answer_one}");
 
+    let answer_two: i32 = get_gear_locations(&grid)
+        .into_iter()
+        .map(|gear| numbers_touching_gear(&grid, gear.0, gear.1))
+        .filter(|numbers| numbers.len() == 2)
+        .map(|numbers| get_part_number(&parts, numbers[0]) * get_part_number(&parts, numbers[1]))
+        .sum();
+
+    println!("Answer 2: {answer_two}");
+}
+
+fn get_part_number(parts: &[Part], location: (i32, i32)) -> i32 {
+    let (row, col) = (location.0 as usize, location.1 as usize);
+    for part in parts.iter() {
+        if part.is_here(row, col) {
+            return part.number;
+        }
+    }
+    0
+}
+
+fn numbers_touching_gear(grid: &[Vec<char>], row: i32, col: i32) -> Vec<(i32, i32)> {
+    let mut touching = vec![];
+    // look left
+    if location_is_digit(&grid, row, col - 1) {
+        touching.push((row, col - 1));
+    }
+    // look right
+    if location_is_digit(&grid, row, col + 1) {
+        touching.push((row, col + 1))
+    }
+    // look up
+    if location_is_digit(&grid, row - 1, col) {
+        touching.push((row - 1, col))
+    } else {
+        // might be two touching check corners
+        // check up left
+        if location_is_digit(&grid, row - 1, col - 1) {
+            touching.push((row - 1, col - 1))
+        }
+        if location_is_digit(&grid, row - 1, col + 1) {
+            touching.push((row - 1, col + 1))
+        }
+    }
+
+    // look down
+    if location_is_digit(&grid, row + 1, col) {
+        touching.push((row + 1, col))
+    } else {
+        // could be two check corners
+        // check down left
+        if location_is_digit(&grid, row + 1, col - 1) {
+            touching.push((row + 1, col - 1))
+        }
+        if location_is_digit(&grid, row + 1, col + 1) {
+            touching.push((row + 1, col + 1))
+        }
+    }
+
+    touching
+}
+
+fn location_is_digit(grid: &[Vec<char>], row: i32, col: i32) -> bool {
+    let row_in_bounds = row >= 0 && (row as usize) < grid.len();
+    let col_in_bounds = col >= 0 && (col as usize) < grid.len();
+
+    if row_in_bounds && col_in_bounds && grid[row as usize][col as usize].is_digit(10) {
+        return true;
+    }
+    false
+}
+
+fn get_gear_locations(grid: &[Vec<char>]) -> Vec<(i32, i32)> {
+    let mut gears = vec![];
+
+    (0..grid.len()).for_each(|i| {
+        (0..grid[i].len()).for_each(|j| {
+            if grid[i][j] == '*' {
+                gears.push((i as i32, j as i32));
+            }
+        });
+    });
+    gears
 }
 
 fn build_grid(contents: &str) -> Vec<Vec<char>> {
@@ -107,10 +196,11 @@ fn build_grid(contents: &str) -> Vec<Vec<char>> {
 fn build_number_list(contents: &str) -> Vec<Part> {
     let mut parts = vec![];
     for (i, line) in contents.lines().enumerate() {
-        let (mut num, mut start, mut end) = (0, 0, 0);
+        let mut end = 0;
         for (j, char) in line.chars().enumerate() {
             if char.is_numeric() && (j == 0 || j > end) {
-                (num, start, end) = parse_number(j, line);
+                let (num, start, e) = parse_number(j, line);
+                end = e;
                 parts.push(Part::new(num, start, end, i));
             }
         }
